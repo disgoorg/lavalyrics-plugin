f4ef319b2c752e01a19cc7ae3a8a50773f994a18
topi314 <git@topi.wtf>
2024-05-14 23:36:22 +0200

    add support for live lyrics

---
+++ events.go
@@ -0,0 +1,68 @@
+package lavalyrics
+
+import (
+	"github.com/disgoorg/disgolink/v3/disgolink"
+	"github.com/disgoorg/disgolink/v3/lavalink"
+	"github.com/disgoorg/snowflake/v2"
+)
+
+const (
+	EventTypeLyricsFound    lavalink.EventType = "LyricsFoundEvent"
+	EventTypeLyricsNotFound lavalink.EventType = "LyricsNotFoundEvent"
+	EventTypeLyricsLine     lavalink.EventType = "LyricsLineEvent"
+)
+
+type LavaLyricsEventListener interface {
+	OnLyricsFound(player disgolink.Player, event LyricsFoundEvent)
+	OnLyricsNotFound(player disgolink.Player, event LyricsNotFoundEvent)
+	OnLyricsLine(player disgolink.Player, event LyricsLineEvent)
+}
+
+type LyricsFoundEvent struct {
+	GuildID_ snowflake.ID `json:"guild_id"`
+}
+
+func (LyricsFoundEvent) Op() lavalink.Op {
+	return lavalink.OpEvent
+}
+
+func (LyricsFoundEvent) Type() lavalink.EventType {
+	return EventTypeLyricsFound
+}
+
+func (e LyricsFoundEvent) GuildID() snowflake.ID {
+	return e.GuildID_
+}
+
+type LyricsNotFoundEvent struct {
+	GuildID_ snowflake.ID `json:"guild_id"`
+}
+
+func (LyricsNotFoundEvent) Op() lavalink.Op {
+	return lavalink.OpEvent
+}
+
+func (LyricsNotFoundEvent) Type() lavalink.EventType {
+	return EventTypeLyricsNotFound
+}
+
+func (e LyricsNotFoundEvent) GuildID() snowflake.ID {
+	return e.GuildID_
+}
+
+type LyricsLineEvent struct {
+	GuildID_ snowflake.ID `json:"guild_id"`
+	Line     Line         `json:"line"`
+}
+
+func (LyricsLineEvent) Op() lavalink.Op {
+	return lavalink.OpEvent
+}
+
+func (LyricsLineEvent) Type() lavalink.EventType {
+	return EventTypeLyricsLine
+}
+
+func (e LyricsLineEvent) GuildID() snowflake.ID {
+	return e.GuildID_
+}
---
+++ rest.go
@@ -0,0 +1,148 @@
+package lavalyrics
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/disgoorg/disgolink/v3/disgolink"
+	"github.com/disgoorg/disgolink/v3/lavalink"
+	"github.com/disgoorg/snowflake/v2"
+)
+
+type Lyrics struct {
+	SourceName string           `json:"sourceName"`
+	Provider   string           `json:"provider"`
+	Text       string           `json:"text"`
+	Lines      []Line           `json:"lines"`
+	Plugin     lavalink.RawData `json:"plugin"`
+}
+
+type Line struct {
+	Timestamp lavalink.Duration `json:"timestamp"`
+	Duration  lavalink.Duration `json:"duration"`
+	Line      string            `json:"line"`
+	Plugin    lavalink.RawData  `json:"plugin"`
+}
+
+// GetCurrentTrackLyrics returns the lyrics of the current track being played in the guild.
+// If the current track has no lyrics, it will return nil.
+func GetCurrentTrackLyrics(ctx context.Context, client disgolink.RestClient, sessionID string, guildID snowflake.ID, skipTrackSource bool) (*Lyrics, error) {
+	rq, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("/v4/sessions/%s/players/%s/track/lyrics", sessionID, guildID), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	q := rq.URL.Query()
+	q.Add("skipTrackSource", fmt.Sprint(skipTrackSource))
+	rq.URL.RawQuery = q.Encode()
+
+	rs, err := client.Do(rq)
+	if err != nil {
+		return nil, err
+	}
+	defer rs.Body.Close()
+
+	if rs.StatusCode < 200 || rs.StatusCode >= 300 {
+		var lavalinkError lavalink.Error
+		if err = json.NewDecoder(rs.Body).Decode(&lavalinkError); err != nil {
+			return nil, err
+		}
+		return nil, lavalinkError
+	}
+
+	if rs.StatusCode == http.StatusNoContent {
+		return nil, nil
+	}
+
+	var l Lyrics
+	if err = json.NewDecoder(rs.Body).Decode(&l); err != nil {
+		return nil, err
+	}
+	return &l, nil
+}
+
+// GetLyrics returns the lyrics of the provided track.
+// If the track has no lyrics, it will return nil.
+func GetLyrics(ctx context.Context, client disgolink.RestClient, track string, skipTrackSource bool) (*Lyrics, error) {
+	rq, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("/v4/lyrics"), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	q := rq.URL.Query()
+	q.Add("track", track)
+	q.Add("skipTrackSource", fmt.Sprint(skipTrackSource))
+	rq.URL.RawQuery = q.Encode()
+
+	rs, err := client.Do(rq)
+	if err != nil {
+		return nil, err
+	}
+	defer rs.Body.Close()
+
+	if rs.StatusCode < 200 || rs.StatusCode >= 300 {
+		var lavalinkError lavalink.Error
+		if err = json.NewDecoder(rs.Body).Decode(&lavalinkError); err != nil {
+			return nil, err
+		}
+		return nil, lavalinkError
+	}
+
+	if rs.StatusCode == http.StatusNoContent {
+		return nil, nil
+	}
+
+	var l Lyrics
+	if err = json.NewDecoder(rs.Body).Decode(&l); err != nil {
+		return nil, err
+	}
+	return &l, nil
+}
+
+func SubscribeLyrics(ctx context.Context, client disgolink.RestClient, sessionID string, guildID snowflake.ID) error {
+	rq, err := http.NewRequestWithContext(ctx, http.MethodPost, fmt.Sprintf("/v4/sessions/%s/players/%s/lyrics/subscribe", sessionID, guildID), nil)
+	if err != nil {
+		return err
+	}
+
+	rs, err := client.Do(rq)
+	if err != nil {
+		return err
+	}
+	defer rs.Body.Close()
+
+	if rs.StatusCode < 200 || rs.StatusCode >= 300 {
+		var lavalinkError lavalink.Error
+		if err = json.NewDecoder(rs.Body).Decode(&lavalinkError); err != nil {
+			return err
+		}
+		return lavalinkError
+	}
+
+	return nil
+}
+
+func UnsubscribeLyrics(ctx context.Context, client disgolink.RestClient, sessionID string, guildID snowflake.ID) error {
+	rq, err := http.NewRequestWithContext(ctx, http.MethodDelete, fmt.Sprintf("/v4/sessions/%s/players/%s/lyrics/subscribe", sessionID, guildID), nil)
+	if err != nil {
+		return err
+	}
+
+	rs, err := client.Do(rq)
+	if err != nil {
+		return err
+	}
+	defer rs.Body.Close()
+
+	if rs.StatusCode < 200 || rs.StatusCode >= 300 {
+		var lavalinkError lavalink.Error
+		if err = json.NewDecoder(rs.Body).Decode(&lavalinkError); err != nil {
+			return err
+		}
+		return lavalinkError
+	}
+
+	return nil
+}
